861022f307296d7ee5ef479a6fad4cb5c5f4d114
Surya Dantuluri <surya.mvhs@gmail.com>
2019-01-23 13:40:57 -0800

    update python

--- unitC/athome.py
+++ unitC/athome.py
@@ -12,23 +12,35 @@ list1.extend((1,3,5))
 list2 = [1,2,3,4]
 
 list3 = list1+list2
-print(list3)
 
-print(3 in list3)
-print(list3.count(3))
-print(list3.index(3))
+print("d) list3 is:",list3)
+
+print("e) list3 contains a 3:",3 in list3)
+
+print("f) list3 contains {0} 3s".format(list3.count(3)))
+
+print("g) The index of the first 3 contained in list3 is",list3.index(3))
+
 first3 = list3.pop(list3.index(3))
-print(first3)
+print("h) first3 =",first3)
+
 list4 = copy.deepcopy(list3)
 list4.sort()
 
-print(list3)
-print(list4)
+print("j) list3 is now:",list3)
+print("j) list4 is:",list4)
+
+print("k) Slice of list3 is:",list3[2:5])
+print("l) Length of list3 is:",len(list3))
+print("m) The max value in list3 is",max(list3))
+print("n) Sorted list3 is:",sorted(list3))
 
 list5 = [list1,list2]
-print(list5)
-print(list5[1][3])
+print("o) list5 is:",list5)
+print("p) Value 4 from list5:",list5[1][3])
 
+print()
+print()
 
 # Second Script
 a = 9
@@ -40,16 +52,22 @@ print("c) binary of a & b =",bin(a&b))
 print("d) binary of a | b =",bin(a|b))
 
 ''' 
-Execution results: 
-[1, 3, 5, 1, 2, 3, 4]
-True
-2
-1
-3
-[1, 5, 1, 2, 3, 4]
-[1, 1, 2, 3, 4, 5]
-[[1, 3, 5], [1, 2, 3, 4]]
-4
+Execution results:
+
+d) list3 is: [1, 3, 5, 1, 2, 3, 4]
+e) list3 contains a 3: True
+f) list3 contains 2 3s
+g) The index of the first 3 contained in list3 is 1
+h) first3 = 3
+j) list3 is now: [1, 5, 1, 2, 3, 4]
+j) list4 is: [1, 1, 2, 3, 4, 5]
+k) Slice of list3 is: [1, 2, 3]
+l) Length of list3 is: 6
+m) The max value in list3 is 5
+n) Sorted list3 is: [1, 1, 2, 3, 4, 5]
+o) list5 is: [[1, 3, 5], [1, 2, 3, 4]]
+p) Value 4 from list5: 4
+
 
 b) binary of a = 0b1001
 b) binary of b = 0b1110
--- unitC/inclass.py
+++ unitC/inclass.py
@@ -1,15 +1,15 @@
 import copy
 
 list1 = [2,4.1,'hello']
-list2 = copy.copy(list1)
+list2 = list1
 list3 = copy.deepcopy(list1)
 
 print("list1 == list2:",list1==list2)
 print("list1 == list3:",list1==list3)
 print("list2 == list3:",list2==list3)
 
-print("list1 is list2:",list2 is list3)
-print("list1 is list3:",list2 is list3)
+print("list1 is list2:",list1 is list2)
+print("list1 is list3:",list1 is list3)
 print("list2 is list3:",list2 is list3)
 
 print("list1 ID:",id(list1))
@@ -20,6 +20,6 @@ list1.append(8)
 list2.insert(1, 'goodbye')
 del list3[0]
 
-print(list1)
-print(list2)
-print(list3)
+print("list1 printed:",list1)
+print("list2 printed:",list2)
+print("list3 printed:",list3)
